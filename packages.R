faa174c33d54df93857ecd58f3fdb43032a6f6a4
Camille Elena <cotsakis1993@gmail.com>
2019-01-15 17:15:03 -0600

    adding freq data

--- packages.R
+++ packages.R
@@ -7,3 +7,11 @@
 ipt <- installed.packages()
 nrow(ipt)
 
+apt <- ipt %>%
+  filter(is.na(Priority)) %>%
+  select(Package, Built)
+
+apt_freqtable <- apt %>%
+  count(Built) %>%
+  mutate(prop = n / sum(n))
+
